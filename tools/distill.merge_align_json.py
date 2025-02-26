55c82e42766d32f96146ef65e5780857f9cace5f
brgew <bge@uw.edu>
2025-02-25 17:15:03 -0800

    Add some tools for testing intermediate JSON files.

---
+++ tools/distill.merge_align_json.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import argparse
+import re
+import os.path
+
+
+pobj = re.compile(r'L([0-9])+')
+
+
+#
+# Read json file.
+#
+def read_json(filename):
+  with open(filename, 'r') as fh:
+    json_data = json.load(fh)
+  return(json_data)
+
+
+def pad_well_col(well_col, zero_pad, id_length):
+  if zero_pad:
+      template = '%%0%sd' % id_length
+  else:
+      template = '%s'
+  col_id = template % (well_col)
+  return col_id
+
+
+# OK
+# Note: the first true well_index value is '0'. The
+#       well_index value < 0 represents no well.
+def index_to_well( well_index, across_row_first ):
+  if( well_index < 0 ):
+    return( (0, 'none'))
+  nrow = 8
+  ncol = 12
+  ipl = int( well_index / 96 )
+  i96 = well_index - ipl * 96
+  if across_row_first:
+      well_row = chr(65 + int(i96 / ncol))
+      well_col = (i96 % ncol) + 1
+  else:
+      well_row = chr(65 + (i96 % nrow))
+      well_col = int(i96 / nrow) + 1
+
+#    well_id = 'P%d-%s%s' % (ipl + 1, well_row, pad_well_col(well_col, zero_pad_col, id_length))
+  well_id = '%s%s' % ( well_row, pad_well_col( well_col, True, 2 ) )
+
+  return( (ipl, well_id ) )
+
+
+def make_index_string( index_list ):
+  """
+  Convert a list of (integer) barcode well indexes to an index string where
+    o  repeated indexes are dropped; that is, keep only distinct indexes
+    o  sequences of counting numbers are expressed as ranges, for example, 5 6 7 8 9 => 5-9
+    o  indexes and index ranges are separated by commas
+  """
+  index_string = ''
+  if( len( index_list ) == 0 ):
+    return( index_string )
+  index_list.sort()
+  index_prev = None
+  index1 = None
+  for i in index_list:
+    if( index_prev ):
+      if( i == index_prev ):
+        continue
+      elif( i > index_prev + 1 ):
+        if( len( index_string ) > 0 ):
+          index_string += ','
+        if( index_prev > index1 ):
+          index_string += '%d-%d' % ( index1, index_prev )
+        else:
+          index_string += '%d' % ( index_prev )
+        index1 = i
+    else:
+      index1 = i
+    index_prev = i
+  # last index in list
+  if( len( index_string ) > 0 ):
+    index_string += ','
+  if( index_prev > index1 ):
+    index_string += '%d-%d' % ( index1, index_prev )
+  else:
+    index_string += '%d' % ( index_prev )
+  return( index_string )
+
+
+def index_string_to_well_string( index_string, across_row_first, show_plate ):
+  well_string = ''
+  for item in index_string.split(','):
+    mobj = re.match( r'^([0-9]+)([-]([0-9]+))?$', item.strip() )
+    if( mobj == None):
+      print('Error: index_string_to_well_string: unable to parse index string', file=sys.stderr)
+      sys.exit(-1)
+    ipl, well = index_to_well(int( mobj.group( 1 ) ) - 1, across_row_first)
+    if( ipl == 0 and not show_plate ):
+      if( len( well_string ) > 0 ):
+        well_string += ','
+      well_string += well
+    else:
+      if( len( well_string ) > 0 ):
+        well_string += ','
+      well_string += 'P%02d-%s' % ( ipl + 1, well )
+    if( mobj.group( 3 ) != None ):
+      ipl, well = index_to_well(int( mobj.group( 3 ) ) - 1, across_row_first)
+      if( ipl == 0 and not show_plate ):
+        well_string += ':%s' % ( well )
+      else:
+        well_string += ':P%02d-%s' % ( ipl + 1, well )
+
+  return(well_string)
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='A program to summarize merge_align pcr indices by sample and process_group.')
+  parser.add_argument('-i', '--input', required=True, default=None, help='Input JSON merge_align filename (required string).')
+  args = parser.parse_args()
+
+
+# [
+#   {
+#     "sample_name": "NSM1323-001",
+#     "out_file": "NSM1323-001.aligned.bam",
+#     "in_dir_list": [
+#       "NSM1323-001_001_024.trimmed",
+#       "NSM1323-001_002_024.trimmed",
+#       "NSM1323-001_003_024.trimmed",
+#       "NSM1323-001_004_024.trimmed",
+#       "NSM1323-001_005_024.trimmed",
+#       "NSM1323-001_006_024.trimmed",
+# ...
+#       "NSM1323-001_095_024.trimmed",
+#       "NSM1323-001_096_024.trimmed"
+#     ]
+#   },
+# ...
+# ]
+
+  sample_name_full_dict = {}
+
+  i = 0
+  json_data = read_json(args.input)
+  for json_item in json_data:
+    i = i + 1
+#    if(i > 10):
+#      break
+
+    sample_name_j = json_item['sample_name']
+    out_file = json_item['out_file']
+    in_dir_list = json_item['in_dir_list']
+
+    sample_name_o = out_file.replace('.aligned.bam', '')
+    if(sample_name_o != sample_name_j):
+      print('Error: distill.merge_align_json.py: inconsistent sample names \'%s\' != \'%s\'' % (sample_name_o, sample_name_j))
+
+    for in_dir in in_dir_list:
+
+      in_dir_parts = in_dir.split('_')
+      sample_name_i = in_dir_parts[0]
+      p7_i = in_dir_parts[1]
+      p5_i = in_dir_parts[2].split('.')[0]
+
+      if(sample_name_i != sample_name_j):
+        print('Error: distill.merge_align_json.py: inconsistent sample names \'%s\' != \'%s\'' % (sample_name_i, sample_name_j))
+
+      if(not sample_name_j in sample_name_full_dict):
+        p7_index_set = set()
+        p5_index_set = set()
+        sample_name_full_dict[sample_name_j] = [p7_index_set, p5_index_set]
+
+      sample_name_full_dict[sample_name_j][0].add(int(p7_i))
+      sample_name_full_dict[sample_name_j][1].add(int(p5_i))
+
+
+  for sample_name in sample_name_full_dict.keys():
+
+    sample_name_full_item = sample_name_full_dict[sample_name]
+    p7_string = make_index_string(list(sample_name_full_item[0]))
+    p5_string = make_index_string(list(sample_name_full_item[1]))
+
+    print('%s  %s  %s  |  %s  %s' % (sample_name, p7_string, p5_string, index_string_to_well_string(p7_string, True, False), index_string_to_well_string(p5_string, False, False)))
+
---
+++ tools/distill.samplesheet_json.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import json
+import re
+import os.path
+
+
+#
+# Read samplesheet json file.
+#
+def read_json(filename):
+  with open(filename, 'r') as fh:
+    json_data = json.load(fh)
+  return(json_data)
+
+
+# def get_pcr_data(json_data):
+#   pcr_data_list= []
+#   for sample_index_dict in json_data['sample_index_list']:
+#     index_ranges = sample_index_dict['ranges'].split(':')
+#     p7_index_list = expand_index_list(index_ranges[1])
+#     p5_index_list = expand_index_list(index_ranges[2])
+#     lane_index_list = expand_index_list(sample_index_dict['lanes'])
+#     p7_file = sample_index_dict['p7_file']
+#     p5_file = sample_index_dict['p5_file']
+#     pcr_data_list.append( {'lane_index_list' : lane_index_list, 'p7_index_list' : p7_index_list, 'p5_index_list' : p5_index_list, 'p7_file' : p7_file, 'p5_file' : p5_file})
+#   return(pcr_data_list)
+
+
+#
+# Expand index list.
+# Example: 1-4,8-12
+# Return a list of distinct indices.
+regex_pattern = r'([0-9]+)([-]([0-9]+))?$'
+def expand_index_list(index_string):
+  index_list = []
+  for index_spec in index_string.split(','):
+    mobj = re.match(regex_pattern, index_spec)
+    if(mobj == None):
+      print('Error: expand_index_list: bad index specification: %s' % (index_spec))
+      sys.exit(-1)
+    index1 = int(mobj.group(1))
+    index2 = index1
+    if(mobj.group(2) != None):
+      index2 = int(mobj.group(3))
+    for one_index in range(index1, index2+1):
+      index_list.append(one_index)
+  return(list(set(index_list)))
+
+
+def pad_well_col(well_col, zero_pad, id_length):
+  if zero_pad:
+      template = '%%0%sd' % id_length
+  else:
+      template = '%s'
+  col_id = template % (well_col)
+  return col_id
+
+
+# OK
+# Note: the first true well_index value is '0'. The
+#       well_index value < 0 represents no well.
+def index_to_well( well_index, across_row_first ):
+  if( well_index < 0 ):
+    return( (0, 'none'))
+  nrow = 8
+  ncol = 12
+  ipl = int( well_index / 96 )
+  i96 = well_index - ipl * 96
+  if across_row_first:
+      well_row = chr(65 + int(i96 / ncol))
+      well_col = (i96 % ncol) + 1
+  else:
+      well_row = chr(65 + (i96 % nrow))
+      well_col = int(i96 / nrow) + 1
+
+#    well_id = 'P%d-%s%s' % (ipl + 1, well_row, pad_well_col(well_col, zero_pad_col, id_length))
+  well_id = '%s%s' % ( well_row, pad_well_col( well_col, True, 2 ) )
+
+  return( (ipl, well_id ) )
+
+
+def make_index_string( index_list ):
+  """
+  Convert a list of (integer) barcode well indexes to an index string where
+    o  repeated indexes are dropped; that is, keep only distinct indexes
+    o  sequences of counting numbers are expressed as ranges, for example, 5 6 7 8 9 => 5-9
+    o  indexes and index ranges are separated by commas
+  """
+  index_string = ''
+  if( len( index_list ) == 0 ):
+    return( index_string )
+  index_list.sort()
+  index_prev = None
+  index1 = None
+  for i in index_list:
+    if( index_prev ):
+      if( i == index_prev ):
+        continue
+      elif( i > index_prev + 1 ):
+        if( len( index_string ) > 0 ):
+          index_string += ','
+        if( index_prev > index1 ):
+          index_string += '%d-%d' % ( index1, index_prev )
+        else:
+          index_string += '%d' % ( index_prev )
+        index1 = i
+    else:
+      index1 = i
+    index_prev = i
+  # last index in list
+  if( len( index_string ) > 0 ):
+    index_string += ','
+  if( index_prev > index1 ):
+    index_string += '%d-%d' % ( index1, index_prev )
+  else:
+    index_string += '%d' % ( index_prev )
+  return( index_string )
+
+
+def index_string_to_well_string( index_string, across_row_first, show_plate ):
+  well_string = ''
+  for item in index_string.split(','):
+    mobj = re.match( r'^([0-9]+)([-]([0-9]+))?$', item.strip() )
+    if( mobj == None):
+      print('Error: index_string_to_well_string: unable to parse index string', file=sys.stderr)
+      sys.exit(-1)
+    ipl, well = index_to_well(int( mobj.group( 1 ) ) - 1, across_row_first)
+    if( ipl == 0 and not show_plate ):
+      if( len( well_string ) > 0 ):
+        well_string += ','
+      well_string += well
+    else:
+      if( len( well_string ) > 0 ):
+        well_string += ','
+      well_string += 'P%02d-%s' % ( ipl + 1, well )
+    if( mobj.group( 3 ) != None ):
+      ipl, well = index_to_well(int( mobj.group( 3 ) ) - 1, across_row_first)
+      if( ipl == 0 and not show_plate ):
+        well_string += ':%s' % ( well )
+      else:
+        well_string += ':P%02d-%s' % ( ipl + 1, well )
+
+  return(well_string)
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='A program to summarize samplesheet.json pcr indices by sample, lane, and process_group.')
+  parser.add_argument('-i', '--input', required=True, default=None, help='Input JSON samplesheet filename (required string).')
+  args = parser.parse_args()
+
+  json_data = read_json(args.input)
+
+  sample_name_full_dict = {}
+
+  sample_index_list = json_data['sample_index_list']
+  
+  i = 0
+  for sample_index_item in sample_index_list:
+    i = i + 1
+#    if(i > 10):
+#      break
+    index_ranges = sample_index_item['ranges'].split(':')
+    sample_name = sample_index_item['sample_id']
+    process_group = sample_index_item['process_group']
+    rt_index_string = index_ranges[0]
+    p7_index_string = index_ranges[1]
+    p5_index_string = index_ranges[2]
+    lane_index_string = sample_index_item['lanes']
+
+    rt_index_list   = expand_index_list(rt_index_string)
+    p7_index_list   = expand_index_list(p7_index_string)
+    p5_index_list   = expand_index_list(p5_index_string)
+    lane_index_list = expand_index_list(lane_index_string)
+
+#    print(sample_index_item)
+#    print('%s  %s  %s  %s' % ('%s-%03d' % (sample_name, int(process_group)), p7_index_string, p5_index_string, lane_index_string))
+
+    sample_name_full = '%s-%03d' % (sample_name, int(process_group))
+    if(not sample_name_full in sample_name_full_dict):
+      rt_index_set = set()
+      p7_index_set = set()
+      p5_index_set = set()
+      lane_index_set = set()
+      sample_name_full_dict[sample_name_full] = [rt_index_set, p7_index_set, p5_index_set, lane_index_set]
+
+    sample_name_full_dict[sample_name_full][0].update(rt_index_list)
+    sample_name_full_dict[sample_name_full][1].update(p7_index_list)
+    sample_name_full_dict[sample_name_full][2].update(p5_index_list)
+    sample_name_full_dict[sample_name_full][3].update(lane_index_list)
+
+#  print(sample_name_full_dict)
+
+  for sample_name_full in sample_name_full_dict.keys():
+    sample_name_full_item = sample_name_full_dict[sample_name_full]
+    rt_string = make_index_string(list(sample_name_full_item[0]))
+    p7_string = make_index_string(list(sample_name_full_item[1]))
+    p5_string = make_index_string(list(sample_name_full_item[2]))
+    lane_string = make_index_string(list(sample_name_full_item[3]))
+
+# Includes rt well information.
+#    print('%s  %s  %s  %s  %s  |  %s  %s  %s' % (sample_name_full, rt_string, p7_string, p5_string, lane_string, index_string_to_well_string(rt_string, True, True), index_string_to_well_string(p7_string, True, False), index_string_to_well_string(p5_string, False, False)))
+
+# Includes p7, p5, and lane information for comparing to the output of the distill.merge_demux_json.py script. The output must be sorted by the first field.
+#    print('%s  %s  %s  %s  |  %s  %s' % (sample_name_full, p7_string, p5_string, lane_string, index_string_to_well_string(p7_string, True, False), index_string_to_well_string(p5_string, False, False)))
+
+# Includes p7 and p5 information for comparing to the output of the distill.merge_align_json.py. The output must be sorted by the first field.
+#    print('%s  %s  %s  |  %s  %s' % (sample_name_full, p7_string, p5_string, index_string_to_well_string(p7_string, True, False), index_string_to_well_string(p5_string, False, False)))
+
+#
+# notes:
+#   o  gather all sample_name+process_group items together into a single string
+#   o  possibly have more than one output type: for merge_demux and for merge_align
+#
