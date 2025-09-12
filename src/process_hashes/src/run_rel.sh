a423c5b5c12fa95f56bd22543b5d29dfb65d2d04
brgew <bge@uw.edu>
2025-09-12 10:16:25 -0700

    Updates.

--- src/process_hashes/src/run_rel.sh
+++ src/process_hashes/src/run_rel.sh
@@ -2,11 +2,17 @@
 
 root_dir="/home/brent/work/data_sets/zebrafish/fastq_hashed/RNA3-072-a"
 
+prg_exe="/home/brent/git/bbi-scirna-analyze/src/process_hashes/target/release/process_hashes"
+
+
 #/home/brent/git/bbi-sci/src/process_hashes/target/release/process_hashes -s $root_dir/timecourse_hash2.txt -f $root_dir/demux_out/SeahubZ01.fq.part1-L001.fastq.gz -k foop -n SeahubZ01
 
-/home/brent/git/bbi-sci/src/process_hashes/target/release/process_hashes -s $root_dir/timecourse_hash2.txt -f $root_dir/demux_out/*.fastq.gz -k foop -n SeahubZ01
+#/home/brent/git/bbi-sci/src/process_hashes/target/release/process_hashes -s $root_dir/timecourse_hash2.txt -f $root_dir/demux_out/*.fastq.gz -k foop -n SeahubZ01
 
 #zcat $root_dir/demux_out/SeahubZ01.fq.part1-L001.fastq.gz | /home/brent/git/bbi-sci/src/process_hashes/target/release/process_hashes -s $root_dir/timecourse_hash2.txt -f - -k foop -n SeahubZ01
 
 #/home/brent/git/bbi-sci/src/process_hashes/target/release/process_hashes -s $root_dir/timecourse_hash2.txt -f SeahubZ01.fq.part1-L001.fastq -k foop -n SeahubZ01
 
+
+# $prg_exe -s $root_dir/timecourse_hash2.txt -b $root_dir/demux_out/SeahubZ01.fq.part1-L001.bam -k foop -n SeahubZ01
+$prg_exe -s $root_dir/timecourse_hash2.txt -b $root_dir/demux_out/*.bam -k foop -n SeahubZ01 -t 2
